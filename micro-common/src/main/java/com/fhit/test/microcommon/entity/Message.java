8560d6466aeebb552ad357c3ed43402dad8e6220
wangtianyi1981 <wangtianyi@tom.com>
2020-03-22 11:21:38 +0800

    first commit

---
+++ micro-common/src/main/java/com/fhit/test/microcommon/entity/Message.java
@@ -0,0 +1,55 @@
+package com.fhit.test.microcommon.entity;
+
+/**
+ * @author wty
+ * @create 2020-03-14 9:52
+ */
+public class Message {
+    /**
+     * 成功或失败
+     */
+    private boolean flag;
+
+    /**
+     * 响应码 200 404 500
+     */
+    private Integer code;
+
+    /**
+     * 请求的结果数据
+     */
+    private Object data;
+
+    public Message() {
+    }
+
+    public Message(boolean flag, Integer code, Object data) {
+        this.flag = flag;
+        this.code = code;
+        this.data = data;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
---
+++ micro-common/src/main/java/com/fhit/test/microcommon/entity/StatusCode.java
@@ -0,0 +1,10 @@
+package com.fhit.test.microcommon.entity;
+
+/**
+ * @author wty
+ * @create 2020-03-14 9:55
+ */
+public class StatusCode {
+    public static final int OK = 20000;
+    public static final int ERROR = 50000;
+}
